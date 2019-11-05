c6e8125352494618d22a16ae4e39511ef4607d35
John Ciardullo <ciardullo.apps@gmail.com>
2019-11-05 12:49:14 -0500

    Remove mock data

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -4,7 +4,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { environment } from '../../environments/environment'
 import { Client } from '../model/client';
-import { CLIENTS } from '../data/clients';
 
 @Injectable({
   providedIn: 'root'
