8f3428dc5779c95cd612e87c0927ebd7a212a706
John Ciardullo <ciardullo.apps@gmail.com>
2021-05-04 11:17:39 -0400

    Begin refactoring tests to eliminate deprecated async function in beforeEach

--- src/app/reports/report.service.spec.ts
+++ src/app/reports/report.service.spec.ts
@@ -1,62 +1,54 @@
+import { HttpHeaders } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed, inject, async } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AuthService } from '../services/auth.service';
 import { monthlyActivityTestData } from '../test/mock-data/monthly-activity-test-data';
+import { MockAuth } from '../test/mock-service/MockAuth';
 
 import { ReportService } from './report.service';
 
 describe('ReportService', () => {
-  // let service: ReportService;
-  let authServiceSpy : any;
+  let service: ReportService;
+  let authService : AuthService;
+  let httpMock: HttpTestingController
 
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({});
-  //   service = TestBed.inject(ReportService);
-  // });
-
-  beforeEach(async(() => {
-    authServiceSpy = jasmine.createSpyObj('AuthService', ['buildHeaders']);
-
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-          HttpClientTestingModule,
+          HttpClientTestingModule
       ],
-      providers: [
-        { provide: AuthService, useValue: authServiceSpy }
+      providers: [ 
+        { provide: AuthService, useClass: MockAuth }
       ]
     })
-  }));
-
-  afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    service = TestBed.inject(ReportService)
+    authService = TestBed.inject(AuthService)
+    httpMock = TestBed.inject(HttpTestingController)
+  });
+  
+  afterEach(() => {
     httpMock.verify();
-  }));
-
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  });
 
   it('should be created', () => {
-    const service: ReportService = TestBed.get(ReportService);
     expect(service).toBeTruthy();
   });
 
-  it('should retrieve monthly activity from http get to backend',
-  inject([HttpTestingController, ReportService],
-    (httpMock: HttpTestingController, service: ReportService) => {
-      // Call the service
-      service.getMonthlyActivity().subscribe(data => {
-        expect(data.length).toBe(2);
-        expect(data[0].monthOfYear).toBe("2017-10");
-        expect(data[1].monthOfYear).toBe("2017-09");
-      });
+  it('should retrieve monthly activity from http get to backend', () => {
+    // Call the service
+    service.getMonthlyActivity().subscribe(data => {
+      expect(data.length).toBe(2);
+      expect(data[0].monthOfYear).toBe("2017-10");
+      expect(data[1].monthOfYear).toBe("2017-09");
+    });
 
-      // Set expectations for the HttpClient mock
-      const req = httpMock.expectOne(`${environment.apiAddress}/monthly-activity`);
-      expect(req.request.method).toEqual('GET');
+    // Set expectations for the HttpClient mock
+    const req = httpMock.expectOne(`${environment.apiAddress}/monthly-activity`);
+    expect(req.request.method).toEqual('GET');
 
-      // Then set the fake data returned by the mock
-      req.flush(monthlyActivityTestData);
-    }
-  ));
+    // Then set the fake data returned by the mock
+    req.flush(monthlyActivityTestData);
+  })
 });
