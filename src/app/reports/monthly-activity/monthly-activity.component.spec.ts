41dd3d14335fc56512282f380cd306f20595a941
John Ciardullo <ciardullo.apps@gmail.com>
2021-05-06 16:48:21 -0400

    Beef up DOM tests - monthly activity report

--- src/app/reports/monthly-activity/monthly-activity.component.spec.ts
+++ src/app/reports/monthly-activity/monthly-activity.component.spec.ts
@@ -1,9 +1,12 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { MatTableModule } from '@angular/material/table';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 import { monthlyActivityTestData } from 'src/app/test/mock-data/monthly-activity-test-data';
+import { forEachChild } from 'typescript';
 import { ReportService } from '../report.service';
 import { MonthlyActivityComponent } from './monthly-activity.component';
 
@@ -46,5 +49,41 @@ describe('MonthlyActivityComponent', () => {
     expect(reportService.getMonthlyActivity).toHaveBeenCalled();
     expect(component.contents.length).toBe(2);
   });
+
+  it('should correctly populate the Total Clients column with contents from Report service', () => {
+    let table: DebugElement[] = fixture.debugElement.queryAll(By.css('.mat-column-totalClients'))
+    expect(table[0].nativeElement.innerHTML).toBe('Clients') // Header Row
+    expect(table[1].nativeElement.innerHTML).toBe(component.contents[0].totalClients.toString()) // First row, total clients value
+    expect(table[2].nativeElement.innerHTML).toBe(component.contents[1].totalClients.toString()) // Second row, total clients value
+  })
+
+  it('should correctly populate the ALL displayed columns with contents from Report service', () => {
+    // Iterate each row of test data
+    for(let rowNum = 0; rowNum < component.contents.length; rowNum++) {
+      // Iterate each column on the report by name
+      component.displayedColumns.forEach(columnName => {
+        // Get one row at a time from the table in the DOM
+        let cell: DebugElement[] = fixture.debugElement.queryAll(By.css(`.mat-column-${columnName}`))
+        // x + 1 below skips the header row
+        expect(cell[rowNum + 1].nativeElement.innerText).toContain(component.contents[rowNum][columnName].toString())
+      });
+    }
+  })
+
+  it('should correctly format the Month Year column anchor tag with the proper URL', () => {
+    let table: DebugElement[] = fixture.debugElement.queryAll(By.css('.mat-column-monthOfYear'))
+    expect(table[0].nativeElement.innerHTML).toBe('Month - Year') // Header Row
+
+    // Assert the href path and the link text
+    table.forEach((element: DebugElement, index: number) => {
+      if(index != 0 && index != table.length - 1) {
+        expect(element.properties.innerText).toBe(component.contents[index - 1].monthOfYear)
+        let links = element.nativeElement.querySelector('a')
+        const year = component.contents[index - 1].monthOfYear.substring(0,4)
+        const month = component.contents[index - 1].monthOfYear.substring(5)
+        expect(links.getAttribute('href')).toContain(`/reports/activity-year-month/${year}/${month}`)
+      }
+    })
+  })
 
 });
--- src/app/test/mock-data/monthly-activity-test-data.ts
+++ src/app/test/mock-data/monthly-activity-test-data.ts
@@ -2,5 +2,5 @@ import { MonthlyActivity } from "src/app/reports/model/monthly-activity";
 
 export const monthlyActivityTestData: MonthlyActivity[] = [
     {"monthOfYear":"2021-05","totalHours":55.75,"totalRevenue":450,"averageRate":10.5,"totalClients":16,"totalAppointments":48},
-    {"monthOfYear":"2021-04","totalHours":5.3333,"totalRevenue":50,"averageRate":12.5,"totalClients":4,"totalAppointments":6}
+    {"monthOfYear":"2021-04","totalHours":5.33,"totalRevenue":50,"averageRate":12.5,"totalClients":4,"totalAppointments":6}
 ]
